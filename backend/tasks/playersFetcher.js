7763168e7ba58a5249dc83ffcd321facfe76c86e
Neifen <neifen.b@gmail.com>
2017-09-22 00:23:01 +0200

    [N4N] * all players

--- backend/tasks/playersFetcher.js
+++ backend/tasks/playersFetcher.js
@@ -3,7 +3,7 @@ class PlayersFetcher{
     fetchAll(){
 
         const fetch = require('node-fetch');
-        fetch('http://api.fantasy.nfl.com/v1/players/stats')
+        fetch('http://api.fantasy.nfl.com/v1/players/stats?statType=seasonProjectedStats')
             .then(function(res) {
                 return res.json();
             }).then(function(json) {
